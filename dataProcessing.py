819ed342b9900fafa8b0b4c40338ba9854a3bcdb
mehulghosal <mehul.ghosal@gmail.com>
2018-11-28 21:46:49 -0500

    third times the charm

---
+++ dataProcessing.py
@@ -0,0 +1,110 @@
+import os, time
+
+#class for data structure so i can kep track of all necessary info
+class Data():
+	#parameters
+	#t is file number; lat is line number; lon is posiiton in line; v is the actual value
+	def __init__(self, time, latitude, longitude, v):
+		self.t = time
+		self.lat = latitude
+		self.lon = longitude
+		self.val = v
+
+	def __str__(self):
+		return str((self.t, self.lat, self.lon, self.val))
+
+#list of files
+files = []
+#lol this will be fun wont it
+#list of all data values that arent 0
+dataList = []
+
+#appends datafiles as file objects to list
+#modifies gloabal var files
+def initFiles(directory):
+	global files
+	#directory = "/home/mehulghosal/code/sciresearch/newdata"
+	for root, dirs, filenames in os.walk(directory):
+		for f in filenames:
+			file = open(directory+f, "r")
+			files.append(file)
+	return files
+
+#sorts files in numeric order 1-480
+def sortFiles(files):
+	#global files
+	newList = []
+	for f in files:
+		newList.append(str(f))
+	#this is a list of file names as strings
+	newList.sort()
+	closeFiles(files)
+	files = []
+	#re-adds each file into files in alphabetical order
+	for fname in newList:
+		#fname is in format: "<_io.TextIOWrapper name='newdata/dtec2dmap_11344_epoch467.txt' mode='r' encoding='UTF-8'>"
+		name = fname[33:-28] #this splices fname --> "newdata....txt"
+		files.append(open("/home/mehulghosal/code/sciresearch/newdata/" + name, "r"))
+	#this should leave files as sorted
+
+#appends the 130,000,000 data objects to dataList
+#and writes to output files
+def readData():
+	global dataList, files
+	#iterates through files
+	t = time.time()
+	for fileIndex in range(len(files)):
+		if fileIndex != 249: continue
+		file = files[fileIndex]
+		#list of strings of each lines
+		lines = file.readlines()
+		#if fileIndex < 414: continue
+
+		outFile = open("/home/mehulghosal/code/sciresearch/output/out" + str(fileIndex+1) + ".txt", "w")
+		for lineIndex in range(len(lines)):
+			line = lines[lineIndex]
+			values = line.split()
+			#iterates through values in line
+			for valIndex in range(len(values)):
+				val = values[valIndex]
+				x = float(val[:-4])
+				if x == 0: continue
+				exp = 10**int(val[-3:])
+				#adding one to all indices to eliminate 0, and go up to 480
+				data = Data(fileIndex + 1, lineIndex + 1, valIndex + 1, x*exp)
+				dataList.append(data)
+				outFile.write(str(data) + "\n")
+		print(fileIndex)
+	print("time required is: " + str(time.time() - t))
+	return dataList
+
+#float vals of the first line in the first file
+#returns list values
+def fLfF():
+	global files
+	#first file as string
+	firstFile = files[0]
+
+	#list of all values in first line
+	firstLine = firstFile.readline().split()
+
+	values = []
+	for val in firstLine:
+		x = float(val[:-4])
+		exponent = 10 ** int(val[-3:])
+		values.append(x*exponent)
+	return values
+
+#closes all files to prevent memory leaks
+#param is list of files
+def closeFiles(files):
+	for file in files:
+		file.close()
+
+if __name__ == '__main__':
+	
+	initFiles("/home/mehulghosal/code/sciresearch/newdata/")
+	sortFiles(files)
+	
+	#readData()
+	closeFiles(files)
---
+++ readme.txt
@@ -0,0 +1,39 @@
+this will be the directory where i will put my science research work
+i might at a fiture date put this on github - who knows
+
+so the original data Dr. P sent me is in data.mat - this is a mat file, and i don't have matlab
+so i processed that data using scipy.io.readmat(), wrote the data to different files as dicts
+this located in dir olddata
+this actually worked, but he sent me new data in text files
+
+the new data is located in the dir newdata
+"There are 480 txt files (each corresponds to each point in time_save).
+Each txt file contains 551 lines (each corresponds to different latitude listed in lat_save)
+and each line contains 501 numbers (each corresponds to different longitude)."
+so im gonna create a new python script just to store the values in some way - idk theres a lot of data points (132,504,480)
+
+
+so from just a quick glance at some of the data files - it looks like theres mostly variation in longitudes of the disturbances
+looks like theres only important stuff in the middle fo the line - so corresponds to middlish long - near equator
+
+11/8 parsed string values into floats  -only for first line of first file
+
+11/9 so the files in the list are out of order - how do i sort them ??
+	at least ik that theyre in the same order every time i run the script
+
+11/12
+lol ok so im just gonna take a real bad attempt at sorting the list
+
+11/22
+so i was writing my readData() functions to append each data point to the list
+i like git stashed a few times, but apparently, it seemed to work, but i think i stopped the function b4 it finished
+i only have files 0-414
+so i can probably run the script for fileIndices >414
+
+ok seems like i got it
+
+got to make some gui? maybe with pygame
+
+11/26 
+lol ok so i dont know/dont want to know pygame that well - so I'll just do the velocity thing 
+im going to find the lat and long average of each file and where they r located --> look in my journal i have notes about how/what im doing
