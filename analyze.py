fefa544ae27ee02991c5838bdb814b66aaea18cc
mehulghosal <mehul.ghosal@gmail.com>
2019-03-15 13:42:56 -0400

    corners work on regular images, but im having trouble getting cv2 to reognize datamap

--- analyze.py
+++ analyze.py
@@ -53,12 +53,50 @@ def mapRange(dataMap):
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_shi_tomasi/py_shi_tomasi.html
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html
 def findCorners(dataMap):
-	# gray = cv2.cvtColor(dataMap,cv2.COLOR_BGR2GRAY)
-	# gray = np.float32(gray)
-	dst = cv2.cornerHarris(dataMap,2,3,0.04) #what do these numbers mean? we may never know
+
+	# img = dataMap
+	# gray = dataMap
+
+	img = cv2.imread("test.png")
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+	gray = np.float32(gray)
+	dst = cv2.cornerHarris(gray,2,3,0.04)
+
+	#result is dilated for marking the corners, not important
+	dst = cv2.dilate(dst,None)
+
 	# Threshold for an optimal value, it may vary depending on the image.
-	img[dataMap>0.01*dst.max()] = [0,0,255]
-	cv2.imshow('dst',dataMap)
+	img[dst>0.01*dst.max()]=[0,0,255]
+
+	cv2.imshow('dst',img)
+	if cv2.waitKey(0) & 0xff == 27:
+		cv2.destroyAllWindows()
+
+#this is the subpixel accuracy thing
+def AAAAAAAA(dataMap):
+	img = dataMap
+	gray = img
+	# find Harris corners
+	gray = np.float32(gray)
+	dst = cv2.cornerHarris(gray,2,3,0.04)
+	dst = cv2.dilate(dst,None)
+	ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+	dst = np.uint8(dst)
+
+	# find centroids
+	ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+	# define the criteria to stop and refine the corners
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+	corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+	# Now draw them
+	res = np.hstack((centroids,corners))
+	res = np.int0(res)
+	img[res[:,1],res[:,0]]=[0,0,255]
+	img[res[:,3],res[:,2]] = [0,255,0]
+	cv2.imshow('dst',img)
 	if cv2.waitKey(0) & 0xff == 27:
 		cv2.destroyAllWindows()
 
@@ -68,10 +106,5 @@ if __name__ == "__main__":
 	# linear list of data objects
 	data, dataMap = (read(files[0]))
 	newMap = mapRange(dataMap)
-	
-	cv2.imshow('dataMap',dataMap)
-	cv2.imshow("newMap", newMap)
-	if cv2.waitKey(0) & 0xff == 27:
-		cv2.destroyAllWindows()
 
-
+	findCorners(newMap)
