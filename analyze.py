4258d66db9c0aae1d9495230e401de9abc97f21a
mehulghosal <mehul.ghosal@gmail.com>
2019-03-20 13:27:19 -0400

    jenfwuc

--- analyze.py
+++ analyze.py
@@ -54,20 +54,17 @@ def mapRange(dataMap):
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html
 def findCorners(dataMap):
 
-	# img = dataMap
-	# gray = dataMap
+	img = dataMap
+	gray = dataMap
 
-	img = cv2.imread("test.png")
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	# img = cv2.imread("test.png")
+	# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 	gray = np.float32(gray)
 	dst = cv2.cornerHarris(gray,2,3,0.04)
 
-	#result is dilated for marking the corners, not important
-	dst = cv2.dilate(dst,None)
-
 	# Threshold for an optimal value, it may vary depending on the image.
-	img[dst>0.01*dst.max()]=[0,0,255]
+	img[dst>0.01*dst.max()]=[255]
 
 	cv2.imshow('dst',img)
 	if cv2.waitKey(0) & 0xff == 27:
@@ -91,20 +88,33 @@ def AAAAAAAA(dataMap):
 	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 	corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
 
+	print(len(corners))
+
 	# Now draw them
-	res = np.hstack((centroids,corners))
-	res = np.int0(res)
-	img[res[:,1],res[:,0]]=[0,0,255]
-	img[res[:,3],res[:,2]] = [0,255,0]
-	cv2.imshow('dst',img)
-	if cv2.waitKey(0) & 0xff == 27:
-		cv2.destroyAllWindows()
+	# res = np.hstack((centroids,corners))
+	# res = np.int0(res)
+	# img[res[:,1],res[:,0]]=[255]
+	# img[res[:,3],res[:,2]] = [255]
+	# cv2.imshow('dst',img)
+	# if cv2.waitKey(0) & 0xff == 27:
+	# 	cv2.destroyAllWindows()
+
+	return corners
 
 if __name__ == "__main__":
 	files = dp.sortFiles(dp.initFiles(directory))
 
 	# linear list of data objects
-	data, dataMap = (read(files[0]))
+	data, dataMap = (read(files[10]))
 	newMap = mapRange(dataMap)
 
-	findCorners(newMap)
+	for i in files:
+		d, dm = read(i)
+		nm = mapRange(dm)
+		x = AAAAAAAA(nm)
+
+	# cv2.imshow('newmap',newMap)
+	# if cv2.waitKey(0) & 0xff == 27:
+	# 	cv2.destroyAllWindows()
+
+	#ok so newmap can be interpreted as an image, but cvtColor doesnt work
