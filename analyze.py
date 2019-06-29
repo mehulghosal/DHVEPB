df68369878e61956fda342343c74a5ad58b2a33d
mehulghosal <mehul.ghosal@gmail.com>
2019-06-29 01:28:00 -0400

    wpijefpijq

--- analyze.py
+++ analyze.py
@@ -44,8 +44,8 @@ def heatMap(dataMap):
 def findCorners(dataMap):
 
 	img = dataMap
-
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	gray = img
+	# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 	corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
 	corners = np.int0(corners)
@@ -55,6 +55,26 @@ def findCorners(dataMap):
 		cv2.circle(img,(x,y),3,255,-1)
 
 	display(img)
+
+def hcd(gray):
+	gray = np.float32(gray)
+	dst = cv2.cornerHarris(gray,2,3,0.04)
+	dst = cv2.dilate(dst,None)
+	ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+	dst = np.uint8(dst)
+
+	# find centroids
+	ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+	# define the criteria to stop and refine the corners
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+	corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+	# Now draw them
+	res = np.hstack((centroids,corners))
+	res = np.int0(res)
+	img[res[:,1],res[:,0]]=[0,0,255]
+	img[res[:,3],res[:,2]] = [0,255,0]
 
 def display(img, name="img"):
 	cv2.imshow(name, img)
@@ -68,13 +88,9 @@ if __name__ == "__main__":
 	# list of data objects
 	data, dataMap = (read(files[0]))
 
-	for i in range(len(files)):
-		_, m = read(files[i])
-		saveAsImage(m, i)
-
 	# display(dataMap)
 	# gray = cv2.cvtColor(dataMap,cv2.COLOR_BGR2GRAY)
-	# findCorners(dataMap)
+	hcd(dataMap)
 
 	# close files
 	closeFiles(files)
