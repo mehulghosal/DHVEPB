8038fba9f1dd46990de94e3aec3734d502fa8ca3
mehulghosal <mehul.ghosal@gmail.com>
2019-10-12 13:16:05 -0400

    inst vels working

--- main.py
+++ main.py
@@ -46,17 +46,25 @@ def crop(m, left=200, top=50, right=500, bottom=400):
 def calc_inst_vel(vectors):
 	l = vectors.shape
 	if not l[1]%2: vectors = vectors[:,:-1,:] # if odd: take out last frame to get even number
-	inst_vels = np.zeros((l[0], l[1], 2))
-	all_v = []
+	inst_vels = []
 
 	for artifact in vectors:
-		inst_vel = np.zeros(l[1], 2)
-
+		inst_vel = []
 		# ignore last frame bc theres nowhere it goes
 		for i in range(len(artifact) - 1):
-			pass
-		
+			pos_0 = artifact[i]
+			pos_1 = artifact[i+1]
+			inst_vel.append([pos_1[0]-pos_0[0], pos_1[1]-pos_0[1]])
+		inst_vels.append(inst_vel)
+	return np.asarray(inst_vels)
 
+def graph_inst(V):
+	i = 0
+	for artifact in V:
+		origin = [0], [0]
+		plt.quiver(*origin, artifact[:,1], artifact[:,0], units='xy')
+		plt.savefig("./inst/inst_" + str(i)+'.png')
+		i+=1
 
 # vectors.shape = (numcorners, numframes, 2)
 # returns avg vels as mag, angle
@@ -75,11 +83,11 @@ def calc_avg_vel(vectors):
 		# avg_vels[i, 0] = np.linalg.norm([dx, dy])/len(vectors[0])
 		# avg_vels[i, 1] = np.degrees(np.arctan([dx, dy]))[0]
 
-	open("avg_vel.txt", "w").write(str(avg_vels))
+	# open("avg_vel.txt", "w").write(str(avg_vels))
 	return avg_vels
 
 # project the instantaneous vectors onto the average vector
-def project():
+def project(avg, inst):
 	pass
 
 
@@ -103,3 +111,4 @@ if __name__ == '__main__':
 	avg_vels = calc_avg_vel(tracks)
 	graph_avgs(avg_vels)
 	inst_vels = calc_inst_vel(tracks)
+	graph_inst(inst_vels)
